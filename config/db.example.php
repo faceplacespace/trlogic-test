accdfdd029a32ac4025f5918a4c716f57e707d58
igornikulin <faceplacespace@gmail.com>
2020-01-17 14:23:17 +0300

    Make db.example.php

---
+++ config/db.example.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'driver' => '',
+    'host' => '',
+    'dbname' => '',
+    'user' => '',
+    'password' => '',
+];
--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'driver' => 'mysql',
-    'host' => '127.0.0.1',
-    'dbname' => 'trlogic-test',
-    'user' => 'root',
-    'password' => 'root',
-];
